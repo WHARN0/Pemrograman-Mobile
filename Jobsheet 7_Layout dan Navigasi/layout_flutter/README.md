3d326aa916ceb3e9918811e64956d5b823afb80d
WHARN0 <variosrio@gmail.com>
2024-09-27 19:33:16 +0700

    Jobsheet 7 Done

---
+++ Jobsheet 7_Layout dan Navigasi/layout_flutter/README.md
@@ -0,0 +1,16 @@
+# Praktikum 7 Layout dan Navigasi
+
+Nama : Bagus Arnovario Wibowo
+NIM : 2241720225
+
+Praktikum 1
+![Screenshot Hasil Praktikum 1](images/Praktikum_1.png)
+
+Praktikum 2
+![Screenshot Hasil Praktikum 2](images/Praktikum_2.png)
+
+Praktikum 3
+![Screenshot Hasil Praktikum 3](images/Praktikum_3.png)
+
+Praktikum 4
+![Screenshot Hasil Praktikum 4](images/Praktikum_4.png)
---
+++ Jobsheet 7_Layout dan Navigasi/layout_flutter/lib/main.dart
@@ -0,0 +1,194 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter layout: Bagus Arnovario Wibowo',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Flutter layout demo'),
+        ),
+        body: const SingleChildScrollView(
+          child: Column(children: [
+            ImageSection(
+              image: 'images/lake.jpg',
+            ),
+            TitleSection(
+              name: 'Wisata Gunung di Batu',
+              location: 'Batu, Malang, Indonesia',
+            ),
+            ButtonSection(),
+            TextSection(
+              description:
+                  'Danau Siblik berada di bawah kaki gunung Bromo di Batu, Malang. '
+                  'Terletak di kawasan perbukitan Batu, Malang, wisata alam ini menawarkan '
+                  'pemandangan luar biasa yang memadukan keindahan gunung dengan danau yang memesona. '
+                  'Terlihat dua tenda berwarna mencolok, seakan mengundang siapa saja untuk berkemah dan menikmati ketenangan alam di sini.\n\n'
+                  'Oleh : Bagus Arnovario Wibowo [2241720225]',
+            ),
+          ]),
+        ),
+      ),
+    );
+  }
+}
+
+class TitleSection extends StatelessWidget {
+  const TitleSection({
+    super.key,
+    required this.name,
+    required this.location,
+  });
+
+  final String name;
+  final String location;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(32),
+      child: Row(
+        children: [
+          Expanded(
+            /* soal 1*/
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                /* soal 2*/
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 8),
+                  child: Text(
+                    name,
+                    style: const TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                Text(
+                  location,
+                  style: TextStyle(
+                    color: Colors.grey[500],
+                  ),
+                ),
+              ],
+            ),
+          ),
+          /* soal 3*/
+          Icon(
+            Icons.star,
+            color: Colors.red[500],
+          ),
+          const Text('41'),
+        ],
+      ),
+    );
+  }
+}
+
+class ButtonSection extends StatelessWidget {
+  const ButtonSection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final Color color = Theme.of(context).primaryColor;
+    return SizedBox(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          ButtonWithText(
+            color: color,
+            icon: Icons.call,
+            label: 'CALL',
+          ),
+          ButtonWithText(
+            color: color,
+            icon: Icons.near_me,
+            label: 'ROUTE',
+          ),
+          ButtonWithText(
+            color: color,
+            icon: Icons.share,
+            label: 'SHARE',
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class ButtonWithText extends StatelessWidget {
+  const ButtonWithText({
+    super.key,
+    required this.color,
+    required this.icon,
+    required this.label,
+  });
+
+  final Color color;
+  final IconData icon;
+  final String label;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(icon, color: color),
+        Padding(
+          padding: const EdgeInsets.only(top: 8),
+          child: Text(
+            label,
+            style: TextStyle(
+              fontSize: 12,
+              fontWeight: FontWeight.w400,
+              color: color,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class TextSection extends StatelessWidget {
+  const TextSection({
+    super.key,
+    required this.description,
+  });
+
+  final String description;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(32),
+      child: Text(
+        description,
+        softWrap: true,
+        textAlign: TextAlign.justify,
+      ),
+    );
+  }
+}
+
+class ImageSection extends StatelessWidget {
+  const ImageSection({super.key, required this.image});
+
+  final String image;
+
+  @override
+  Widget build(BuildContext context) {
+    return Image.asset(
+      image,
+      width: 600,
+      height: 240,
+      fit: BoxFit.cover,
+    );
+  }
+}
