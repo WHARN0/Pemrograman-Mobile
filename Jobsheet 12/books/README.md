257080229340ec597044b6b541ec46ec76ad81b3
WHARN0 <variosrio@gmail.com>
2024-11-08 09:42:07 +0700

    W12: Soal 5

--- Jobsheet 12/books/README.md
+++ Jobsheet 12/books/README.md
@@ -1,11 +1,18 @@
 # Jobsheet 12 Pemrograman Asynchronous
 
 Nama : Bagus Arnovario Wibowo<br/>
-NIM  : 2241720225
+NIM  : 2241720225<br/>
 
 Hasil Langkah 4 Praktikum 1 (Soal 2)<br/>
 ![Screenshot Hasil Langkah 4 Praktikum 1](images/Screenshot_Langkah_4_P1.png)<br/>
 
 Hasil Praktikum 1 Mengunduh Data dari Web Service (API) (Soal 3)<br/>
-![Screenshot Hasil Praktikum 1](images/Hasil_P1.gif)<br/>
+![Screenshot Hasil Praktikum 1](images/Gif_Hasil_P1.gif)<br/>
 
+Hasil Praktikum 2 Menggunakan await/async untuk menghindari callbacks (Soal 4)<br/>
+![Screenshot Hasil Praktikum 2](images/Gif_Hasil_P2.gif)<br/>
+
+Hasil Langkah 4 Praktikum 3 (Soal 5)<br/>
+![Screenshot Hasil Langkah 4 Praktikum 3](images/Gif_Langkah_4_P2.gif)<br/>
+
+Hasil 
--- Jobsheet 12/books/lib/main.dart
+++ Jobsheet 12/books/lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'dart:async';
 import 'package:http/http.dart';
 import 'package:http/http.dart' as http;
+import 'package:async/async.dart';
 
 void main() {
   runApp(const MyApp());
@@ -32,6 +33,43 @@ class FuturePage extends StatefulWidget {
 
 class _FuturePageState extends State<FuturePage> {
   String result = '';
+  late Completer completer;
+
+  Future getNumber() {
+    completer = Completer<int>();
+    calculate();
+    return completer.future;
+  }
+
+  Future calculate() async {
+    await Future.delayed(const Duration(seconds: 5));
+    completer.complete(42);
+  }
+
+  Future<int> returnOneAsync() async {
+    await Future.delayed(const Duration(seconds: 3));
+    return 1;
+  }
+
+  Future<int> returnTwoAsync() async {
+    await Future.delayed(const Duration(seconds: 3));
+    return 2;
+  }
+
+  Future<int> returnThreeAsync() async {
+    await Future.delayed(const Duration(seconds: 3));
+    return 3;
+  }
+
+  Future count() async {
+    int total = 0;
+    total = await returnOneAsync();
+    total += await returnTwoAsync();
+    total += await returnThreeAsync();
+    setState(() {
+      result = total.toString();
+    });
+  }
 
   Future<Response> getData() async {
     const authority = 'www.googleapis.com';
@@ -52,15 +90,25 @@ class _FuturePageState extends State<FuturePage> {
             const Spacer(),
             ElevatedButton(
               child: const Text('GO!'),
+              // onPressed: () {
+              //   setState(() {});
+              //   getData().then((value) {
+              //     result = value.body.toString().substring(0, 450);
+              //     setState(() {});
+              //   }).catchError((_) {
+              //     result = 'An error occured';
+              //     setState(() {});
+              //   });
+              // },
+              // onPressed: () {
+              //   count();
+              // },
               onPressed: () {
-                setState(() {});
-                getData().then((value) {
-                  result = value.body.toString().substring(0, 450);
-                  setState(() {});
-                }).catchError((_) {
-                  result = 'An error occured';
-                  setState(() {});
-                });
+                getNumber().then((value) {
+                  setState(() {
+                    result = value.toString();
+                  });
+                }); 
               },
             ),
             const Spacer(),
