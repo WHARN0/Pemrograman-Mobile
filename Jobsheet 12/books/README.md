8fd512081c1dc78d2ef9db57dddb0fea8fbf1c2a
WHARN0 <variosrio@gmail.com>
2024-11-08 08:23:38 +0700

    W12: Soal 2

---
+++ Jobsheet 12/books/README.md
@@ -0,0 +1,8 @@
+# Jobsheet 12 Pemrograman Asynchronous
+
+Nama : Bagus Arnovario Wibowo<br/>
+NIM  : 2241720225
+
+Hasil Langkah 4 Praktikum 1 (Soal 2)
+![Screenshot Hasil Langkah 4 Praktikum 1](images/Screenshot_Langkah_4_P1.png)<br/>
+
---
+++ Jobsheet 12/books/lib/main.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
+import 'package:http/http.dart';
+import 'package:http/http.dart' as http;
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Future Demo Arno',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: const FuturePage(),
+    );
+  }
+}
+
+class FuturePage extends StatefulWidget {
+  const FuturePage({super.key});
+
+  @override
+  State<FuturePage> createState() => _FuturePageState();
+}
+
+class _FuturePageState extends State<FuturePage> {
+  String result = '';
+
+  Future<Response> getData() async {
+    const authority = 'www.googleapis.com';
+    const path = '/books/v1/volumes/qCLFw5injs8C';
+    Uri url = Uri.http(authority, path);
+    return http.get(url);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Back from the Future'),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            const Spacer(),
+            ElevatedButton(
+              child: const Text('GO!'),
+              onPressed: () {},
+            ),
+            const Spacer(),
+            Text(result),
+            const Spacer(),
+            const CircularProgressIndicator(),
+            const Spacer(),
+          ],
+        ),
+      ),
+    );
+  }
+}
