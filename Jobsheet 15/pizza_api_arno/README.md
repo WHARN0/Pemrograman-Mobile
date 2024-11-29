dce6dd7964efa2c59aa94f66825a8109789167a7
WHARN0 <variosrio@gmail.com>
2024-11-29 09:05:31 +0800

    Praktikum 1: Designing an HTTP client and getting data | Done

---
+++ Jobsheet 15/pizza_api_arno/README.md
@@ -0,0 +1,6 @@
+# pizza_api_arno
+
+
+
+
+![Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
---
+++ Jobsheet 15/pizza_api_arno/android/app/src/main/kotlin/com/example/pizza_api_arno/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.pizza_api_arno
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
